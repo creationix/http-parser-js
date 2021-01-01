273bd3b5f2b6c6159e6f99dfbee14497a9caa682
Jimb Esser <wasteland@gmail.com>
2021-01-01 14:45:08 -0800

    Testing new github actions pipeline

--- tests/parallel/test-is-monkeypatched.js
+++ tests/parallel/test-is-monkeypatched.js
@@ -5,6 +5,8 @@ var assert = require('assert');
 var http_common = require('_http_common');
 var http_parser_js = require('../../http-parser.js')
 
+assert(false);
+
 if (parseInt(process.versions.node) <= 10) {
 	// not exported, no easy way to check, but we know it works there
 } else {
