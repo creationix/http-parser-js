9791bb9fd8c77fa51414f6c20497a56f1ea8b3cb
Alexander Prinzhorn <alexander@prinzhorn.it>
2022-01-28 14:43:00 +0100

    comment about async

--- standalone-example.js
+++ standalone-example.js
@@ -44,6 +44,10 @@ function parseRequest(input) {
     complete = true;
   };
 
+  // Since we are sending the entire Buffer at once here all callbacks above happen synchronously.
+  // The parser does not do _anything_ asynchronous.
+  // However, you can of course call execute() multiple times with multiple chunks, e.g. from a stream.
+  // But then you have to refactor the entire logic to be async (e.g. resolve a Promise in kOnMessageComplete and add timeout logic).
   parser.execute(input);
   parser.finish();
 
@@ -102,6 +106,10 @@ function parseResponse(input) {
     complete = true;
   };
 
+  // Since we are sending the entire Buffer at once here all callbacks above happen synchronously.
+  // The parser does not do _anything_ asynchronous.
+  // However, you can of course call execute() multiple times with multiple chunks, e.g. from a stream.
+  // But then you have to refactor the entire logic to be async (e.g. resolve a Promise in kOnMessageComplete and add timeout logic).
   parser.execute(input);
   parser.finish();
 
